4c7751567b53a3086e1515651b08615cf8190285
Emir Bayraktar <96651996+Bayrktr@users.noreply.github.com>
2024-11-30 01:32:05 +0300

    Sarrus method is done Cramer method not done yet

---
+++ home_work/cramer_method.py
@@ -0,0 +1,94 @@
+import math
+
+import numpy as np
+
+
+def sarrus_method_det(array):
+    array_column_lenght = array.shape[1]
+    array_row_lenght = array.shape[0]
+
+    if array_column_lenght != 3:
+        print('Sarrus method cannot be applied')
+        return
+
+    extended_array = array.copy()
+
+    extend_size = array_column_lenght - 1
+
+    for j in range(extend_size):
+        extended_array = np.vstack([extended_array, array[j]])
+
+    print(extended_array)
+
+    extended_array_column_lenght = extended_array.shape[0]
+    extended_array_row_lenght = extended_array.shape[1]
+
+    total_list_number = (extended_array_column_lenght - extended_array_row_lenght) + 1
+
+    positive_total_numbers_list = []
+
+    for x in range(total_list_number):
+        total_index = x
+        numbers_list = []
+        for y in range(extended_array_column_lenght):
+            if (y + x) >= extended_array_column_lenght:
+                break
+
+            row = extended_array[y + x]
+
+            for z in range(extended_array_row_lenght):
+                item = row[z]
+                if (x + z) == total_index:
+                    total_index += (extended_array_row_lenght - 2)
+                    numbers_list.append(int(item))
+                    break
+        positive_total_numbers_list.append(numbers_list)
+
+    print(positive_total_numbers_list)
+
+    negative_total_numbers_list = []
+
+    for x in range(total_list_number):
+        total_index = x
+        numbers_list = []
+        for y in range(extended_array_column_lenght):
+            if (y + x) >= extended_array_column_lenght:
+                break
+
+            row = extended_array[y + x]
+
+            for z in range(extended_array_row_lenght - 1, -1, -1):
+                item = row[z]
+                if (x + (extended_array_row_lenght - 1 - z)) == total_index:
+                    total_index += (extended_array_row_lenght - 2)
+                    numbers_list.append(int(item))
+                    break
+        negative_total_numbers_list.append(numbers_list)
+
+    total_sum_positive = sum(math.prod(sublist) for sublist in positive_total_numbers_list)
+
+    total_sum_negative = sum(math.prod(sublist) for sublist in negative_total_numbers_list)
+
+    det = (total_sum_positive - total_sum_negative)
+
+    return det
+
+def cramer_method_result(array,determinant):
+    return
+
+
+
+
+
+cramer_array = np.array([[1, 3, 5],
+                         [2, 6, 7],
+                         [3, 6, 4]])
+
+if cramer_array.shape[0] != cramer_array.shape[1]:
+    print('not a square matrix')
+
+det = sarrus_method_det(cramer_array)
+
+print(det)  # -9 result
+
+cramer_method_result(cramer_array,det)
